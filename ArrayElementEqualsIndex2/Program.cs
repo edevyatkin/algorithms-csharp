ae24ada185fa0d892b0422d196802f27cb3c8437
Evgeniy Devyatkin <mail@edevyatkin.com>
2016-07-06 13:23:44 +0300

    Migration to .Net Core

---
+++ ArrayElementEqualsIndex2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Input: 
+ * Array of distinct integers
+ * 
+ * Problem: 
+ * Find index of element which equals its index in array, if it exists.
+ * 
+ * Solution using binary search of zero difference 
+ * between the element and its index in array
+ */
+
+namespace ArrayElementEqualsIndex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No input data");
+                ShowUsage();
+                return;
+            }
+
+            int[] arr = Array.Empty<int>();
+            try
+            {
+                arr = args.Select(int.Parse).ToArray();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Incorrect input data");
+                ShowUsage();
+                return;
+            }
+
+            int resultIndex = -1;
+
+            // Binary search of zero diff
+            int low = 0;
+            int high = args.Length - 1;
+            while (low <= high)
+            {
+                int index = low + ((high - low) >> 1);
+                if (arr[index] - index > 0)
+                {
+                    high = index - 1;
+                }
+                else if (arr[index] - index < 0)
+                {
+                    low = index + 1;
+                }
+                else
+                {
+                    resultIndex = index;
+                    break;
+                }
+            }
+
+            if (resultIndex >= 0)
+            {
+                Console.WriteLine("Element equals index at index: " + resultIndex);
+            }
+            else
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage: [int] [int] ... [int]");
+        }
+    }
+}
---
+++ BallsAndSkyscraper2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+/*
+ * There is a number of glass balls and a skyscraper.
+ * If we throw a ball from the floor higher than a specific floor the ball will have broken.
+ * 
+ * Goal: Find out minimum count of throws in worst case to determine that specific floor
+ * 
+ * Solution using dynamic programming
+ */
+
+namespace BallsAndSkyscraper2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: [balls] [floors]");
+                Console.WriteLine("balls - integer number, floors - integer number");
+                return;
+            }
+            int balls;
+            int.TryParse(args[0], out balls);
+            int floors;
+            int.TryParse(args[1], out floors);
+            if (balls == 0 || floors == 0)
+            {
+                Console.WriteLine("Both arguments must be integer numbers");
+                return;
+            }
+            Console.WriteLine("Glass balls: " + balls);
+            Console.WriteLine("Floors: " + floors);
+            int catches = CalculateMinThrows(balls, floors);
+            Console.WriteLine("Minimum count of throws in worst case: " + catches);
+        }
+
+        private static int CalculateMinThrows(int balls, int floors)
+        {
+            if (balls == 1 || floors == 1 || floors == 2)
+            {
+                return floors;
+            }
+
+            int[,] dp = new int[balls, floors + 1];
+            for (int i = 1; i <= floors; i++)
+            {
+                dp[0, i] = i;
+            }
+
+            for (int ball = 1; ball < balls; ball++)
+            {
+                dp[ball, 1] = 1;
+                dp[ball, 2] = 2;
+                for (int maxFloor = 3; maxFloor <= floors; maxFloor++)
+                {
+                    int minFloor = floors;
+                    for (int currentFloor = 1; currentFloor <= maxFloor; currentFloor++)
+                    {
+                        int worstFloorIfBroken = dp[ball - 1, currentFloor - 1];
+                        int worstFloorIfSaved = dp[ball, maxFloor - currentFloor];
+                        int optimalFloor = Math.Max(worstFloorIfBroken, worstFloorIfSaved);
+                        if (optimalFloor < minFloor)
+                        {
+                            minFloor = optimalFloor;
+                        }
+                    }
+                    dp[ball, maxFloor] = minFloor + 1;
+                }
+            }
+
+            return dp[balls - 1, floors];
+        }
+    }
+}
